fb66ca4985735d9f06a3480e5ed23065b02366ee
seireitou <ausis@mail.com>
2021-02-25 19:02:23 -0600

    commit tree 45b72be9e730d04a051e1b4ac1f2be1815b077b7

--- app.rb
+++ app.rb
@@ -2,5 +2,44 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number ** 2}"
+  end
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    
+    "#{@phrase * @number}"
+  end
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+
+  get '/:operation/:number1/:number2' do
+    if params[:operation].include? "add"
+      sum = params[:number1].to_i + params[:number2].to_i
+      "#{sum}"
+    elsif params[:operation].include? "subtract"
+      sum = params[:number1].to_i - params[:number2].to_i
+      "#{sum}"
+    elsif params[:operation].include? "multiply"
+      sum = params[:number1].to_i * params[:number2].to_i
+      "#{sum}"
+    elsif params[:operation].include? "divide"
+      # binding.pry
+  
+      sum = params[:number1].to_i / params[:number2].to_i
+      "#{sum}"
+    end
+  end
+  
+
 
 end
